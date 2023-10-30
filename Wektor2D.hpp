d1d04848ea6ab97099f0cdd0c74b0ad3d1362a61
Tomasz Witowski <134285475+Tommooo77@users.noreply.github.com>
2023-10-30 05:43:56 +0100

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,55 @@
-class Wektor2D
+#include <iostream>
+using namespace std;
+
+class Informer
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Informer() { cout << "Utworzylem obiekt" << endl; }
+    ~Informer() { cout << "Zniszczylem obiekt" << endl; }
 };
+
+class wektor2D
+{
+public:
+    wektor2D(double a, double b)
+    {
+        x = a;
+        y = b;
+        print();
+    }
+
+    ~wektor2D()
+    {
+        print();
+    }
+    double norm(double x, double y)
+    {
+        return sqrt(x * x + y * y);
+    }
+
+    void print()
+    {
+        cout << "Wspolrzedne wektora[ " << x << "," << y << "]" << endl;
+    }
+    void setX(int a) {
+        x = a;
+    }
+    void setY(int b) {
+        y = b;
+    }
+    double getX() {
+        return x;
+    }
+    double getY() {
+        return y;
+    }
+
+private:
+    double x;
+    double y;
+    Informer informator;
+};
+
+wektor2D operator+(wektor2D A, wektor2D B) {
+    return wektor2D{ A.getX() + B.getX(),A.getY() + B.getY() };
+}
